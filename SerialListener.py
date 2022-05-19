2b940d8ff5e37dcbcc7576620b6bad31709c5536
Максим Зайцев <maksim.zaytsev@playrix.com>
2022-05-20 01:32:17 +0700

    Add dumping CSV file dumping

--- SerialListener.py
+++ SerialListener.py
@@ -1,24 +1,30 @@
+import os
+
 import serial
 import serial.tools.list_ports
 import sys
 import time
 import datetime
-
-ports = serial.tools.list_ports.comports()
-
-idx = 0
-for port, desc, hwid in sorted(ports):
-    idx += 1
-    print("{}. {}: {} [{}]".format(idx, port, desc, hwid))
-
-targetPortIdx = int(input("Enter the number of COM port info line: ")) - 1
+import csv
 
 
-def GetTemperature(rawValue: str):
-    return int(rawValue.split("=9600A71")[1][:-3])
+def SplitLine(line: str) -> list:
+    expectedPrefix = "=9600A71"
+    if not line.startswith(expectedPrefix):
+        print("Warning: unexpected line prefix in next line: {}".format(line))
+        return [line, -1, "???"]
+    prefLen = len(expectedPrefix)
+    return [line[:prefLen], int(line[prefLen:prefLen + 4]), line[prefLen + 4:]]
 
 
-def ListendCOMPort(port:str):
+def ProcessSerialDataLine(line):
+    dataRow = SplitLine(line)
+    curdt = datetime.datetime.fromtimestamp(time.time()).isoformat()
+    print(curdt, dataRow)
+    return [curdt] + dataRow
+
+
+def ListendCOMPort(port: str):
     data = []
     needStopThread = False
 
@@ -28,9 +34,10 @@ def ListendCOMPort(port:str):
         while not needStopThread:
             if (serialPort.in_waiting > 0):
                 serialString = serialPort.read_all()
-                temperature = GetTemperature(serialString.decode('utf8', errors='ignore'))
-                curdt = datetime.datetime.fromtimestamp(time.time())
-                print(curdt.isoformat(), temperature)
+                for line in serialString.decode('utf8', errors='ignore').split('\n'):
+                    if len(line) == 0:
+                        continue
+                    data.append(ProcessSerialDataLine(line))
         else:
             serialPort.close()
 
@@ -41,7 +48,38 @@ def ListendCOMPort(port:str):
     needStopThread = True
     thread.join()
 
-    return data
+    return ["Time", "Prefix", "Temperature", "Postfix"], data
 
-from pprint import pprint
-pprint(ListendCOMPort(ports[targetPortIdx][0]))
+
+def WriteToCSV(dataRowsNames, dataRows, filename):
+    with open(filename, 'w', newline='') as outFile:
+        writer = csv.writer(outFile, dialect='excel')
+        writer.writerow(dataRowsNames)
+        writer.writerows(dataRows)
+
+
+def main():
+    ports = serial.tools.list_ports.comports()
+
+    idx = 0
+    for port, desc, hwid in sorted(ports):
+        idx += 1
+        print("{}. {}: {} [{}]".format(idx, port, desc, hwid))
+
+    targetPortIdx = int(input("Enter the number of COM port info line: ")) - 1
+    dataRowsNames, dataRows = ListendCOMPort(ports[targetPortIdx][0])
+
+    tempOutFilename = "Result.csv"
+    WriteToCSV(dataRowsNames, dataRows, tempOutFilename)
+    while 1:
+        resultFilename = input("Enter a name for the result .csv file (without extension): ") + ".csv"
+        if os.path.exists(resultFilename):
+            print("File with this name already exists: {}. Please, enter another one.".format(resultFilename))
+            continue
+        break
+    os.rename(tempOutFilename, resultFilename)
+    print("Result filename is: {}".format(resultFilename))
+
+
+if __name__ == '__main__':
+    main()
